278654d9da2a05cc8510ae38b544854c35cb9b6d
QuemoZem <fran.tucmascotas@gmail.com>
2023-05-07 23:37:20 -0300

    ME ESTA VOLVIENDO LOCO

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,3 +1,5 @@
+import styles from './Card.module.css'
+
 export default function Card({
   name,
   status,
@@ -8,14 +10,14 @@ export default function Card({
   onclose,
 }) {
   return (
-    <div>
+    <div className= {styles.listItem}>
       <button onClick={onclose}>X</button>
+      <img src={image} alt="" />
       <h2>{name}</h2>
       <h2>{status}</h2>
       <h2>{species}</h2>
       <h2>{gender}</h2>
       <h2>{origin.name}</h2>
-      <img src={image} alt="" />
     </div>
   );
 }
